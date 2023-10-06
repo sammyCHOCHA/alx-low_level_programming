e04db22d047d5aabcab73398ec5dda4194a57a64
sammyCHOCHA <sammychoo210@gmail.com>
2023-10-06 15:38:16 -0700

    playing the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	    char c = 'a';
+
+	        while (c <= 'z')
+			    {
+				            putchar(c);
+					            c++;
+						        }
+
+		    putchar('\n');  /* Print a newline after the alphabet */
+		        return 0;
+}
+
