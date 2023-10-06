12dbb3e0bc7f8ad1fa68e35b46ca5a7de28cbc4c
sammyCHOCHA <sammychoo210@gmail.com>
2023-10-05 19:14:53 -0700

    changes made and corrected using betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main(void) {
-	    puts("Programming is like building a multilingual puzzle");
-	        return 0;  /* Program executed successfully */
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+		printf("Programming is like building a multilingual puzzle\n");
+			return (0);
 }
-
