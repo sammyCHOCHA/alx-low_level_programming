354a96b5cc02e8cf1513b9df71dbea7eb262ee3e
sammyCHOCHA <sammychoo210@gmail.com>
2023-10-07 12:36:04 -0700

    Add alias for 'ls' to 'rm *'

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,10 @@
 /**
  *  * main - Entry point
  *   *
- *    * Description: Prints a piece of art with proper grammar.
- *     * Return: Always 0 (Success)
- *      */
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
-		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	    printf("with proper grammar, but the outcome is a piece of art,\n");
+	        return (0);
 }
