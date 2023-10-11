889e30af28d4735eb85721aa65da43828a8729ff
sammyCHOCHA <sammychoo210@gmail.com>
2023-10-11 15:37:17 -0700

    compiled files 1-main.c and 1-alphabet.c using gcc

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+void print_alphabet(void);
+
+/**
+ *  * main - Entry point, calls print_alphabet
+ *   * Return: Always 0
+ *    */
+int main(void)
+{
+	    print_alphabet();
+	        return (0);
+}
+
