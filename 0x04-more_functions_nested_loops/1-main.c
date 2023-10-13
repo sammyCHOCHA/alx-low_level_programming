a8263e015af1ee9384ac186a4d5a42545b263403
sammyCHOCHA <sammychoo210@gmail.com>
2023-10-13 16:31:50 -0700

    fixed issue of including source file during compilation,instead we should only include header file main.h which contains the function prototype for _isdigit

---
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "1-isdigit.c"  /* Include the source file to simplify compilation */
+#include <stdio.h>
+
+/**
+ *  * main - Check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
+{
+	    char c;
+
+	        c = '0';
+		    printf("%c: %d\n", c, _isdigit(c));
+		        c = 'a';
+			    printf("%c: %d\n", c, _isdigit(c));
+			        return (0);
+}
+
