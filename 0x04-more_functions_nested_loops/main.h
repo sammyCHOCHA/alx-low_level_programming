e3577b3ad01188ea07fe45b1f595efcee5e20a53
sammyCHOCHA <sammychoo210@gmail.com>
2023-10-13 19:34:52 -0700

    Your commit message here

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);  /* Declaration of _putchar */
-
-void print_numbers(void);
+int _putchar(char c);
+void print_most_numbers(void);
 
 #endif /* MAIN_H */
 
