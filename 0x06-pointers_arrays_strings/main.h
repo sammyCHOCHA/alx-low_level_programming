c8a6ca113f0ac8fb104f3e4ac7a321e82c052aae
sammyCHOCHA <sammychoo210@gmail.com>
2023-10-20 18:57:22 -0700

    include function for the string_toupper function

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 /* Function prototypes for string manipulation functions */
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
+char *string_toupper(char *str);
 
 #endif /* MAIN_H */
 
