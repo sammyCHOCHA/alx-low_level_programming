dc67c498b77d858edd95695b300d29652bf5aaa0
sammyCHOCHA <sammychoo210@gmail.com>
2023-10-20 19:52:58 -0700

    updated print_buffer function prototype.

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  * print_buffer - Prints a buffer
+ *   * @b: The buffer to print
+ *    * @size: The size of the buffer
+ *     */
+void print_buffer(char *b, int size)
+{
+	    int i, j;
+	        
+	        for (i = 0; i < size; i += 10)
+			    {
+				            printf("%08x: ", i);
+					            
+					            for (j = 0; j < 10; j++)
+							            {
+									                if (i + j < size)
+												                printf("%02x", b[i + j]);
+											            else
+													                    printf("  ");
+												                
+												                if (j % 2 == 1)
+															                printf(" ");
+														        }
+						            
+						            for (j = 0; j < 10; j++)
+								            {
+										                if (i + j < size)
+													            {
+															                    if (b[i + j] >= 32 && b[i + j] <= 126)
+																		                        printf("%c", b[i + j]);
+																	                    else
+																				                        printf(".");
+																			                }
+												            else
+														                    printf(" ");
+													            }
+							            
+							            printf("\n");
+								        }
+		    
+		    if (size <= 0)
+			            printf("\n");
+}
+
